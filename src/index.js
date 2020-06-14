d3a46c0e4309b2c9779e4f15271cce75e3ad5b21
Kenneth <kendevops@gmail.com>
2020-06-14 23:05:14 +0100

    Applied AWS Configure to the exports

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import Amplify from "aws-exports";
+import aws_exports from "./aws-exports";
+
+Amplify.configure(aws_exports);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
