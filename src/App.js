0851cf26361911b6038c215cdc08d1682406c61e
Kenneth <kendevops@gmail.com>
2020-06-16 14:24:18 +0100

    Added Authentication

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import { AmplifyAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 
 const App = () => {
   return (
-    <AmplifyAuthenticator>
+    <AmplifyAuthenticator
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: "auto",
+      }}
+    >
       <div>
         <h1>APP</h1>
         <AmplifySignOut />
